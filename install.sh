6d5c7282d1163900692783d9349e335f445ca5cc
Daniel Tschan <tschan@puzzle.ch>
2016-12-20 22:00:13 +0100

    Upgrade to Scratch 2 version 453.

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
+SCRATCH2_VERSION=453
+
 yum -y install \
     ld-linux.so.2 \
     gtk2-devel.i686 \
@@ -26,12 +28,12 @@ mkdir -p /home/scratchy/Downloads /home/scratchy/Documents
 cd /home/scratchy/Downloads
 curl -L -O http://airdownload.adobe.com/air/lin/download/2.6/AdobeAIRInstaller.bin
 chmod +x AdobeAIRInstaller.bin
-curl -L -O https://scratch.mit.edu/scratchr2/static/sa/Scratch-451.air
-chmod +x Scratch-451.air
+curl -L -O https://scratch.mit.edu/scratchr2/static/sa/Scratch-${SCRATCH2_VERSION}.air
+chmod +x Scratch-${SCRATCH2_VERSION}.air
 
 Xvfb :1 &
 export DISPLAY=:1
-/home/scratchy/Downloads/AdobeAIRInstaller.bin -silent -eulaAccepted /home/scratchy/Downloads/Scratch-451.air
+/home/scratchy/Downloads/AdobeAIRInstaller.bin -silent -eulaAccepted /home/scratchy/Downloads/Scratch-${SCRATCH2_VERSION}.air
 cd /opt/Scratch\ 2/share/locale
 patch </tmp/de-wedo2.po.patch
 ln -s /opt/Scratch\ 2/bin/Scratch\ 2 /usr/bin/scratch
